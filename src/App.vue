a53073adb5ae0c235c8633136ad58165793b6f50
ibtehalahmed <ibtehal599@gmail.com>
2017-04-04 17:33:01 +0200

    add list and details

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,32 @@
 <template>
-  <div id="app">
-    <h1>Hello</h1>
-    <app-servers></app-servers>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <div class="row">
+      <servers></servers>
+      <app-server-details></app-server-details>
+    </div>
+    <hr>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import Header from './components/Shared/Header.vue';
+  import Footer from './components/Shared/Footer.vue';
+  import Servers from './components/Server/Servers.vue';
+  import ServerDetails from './components/Server/ServerDetails.vue';
+
+  export default {
+    components: {
+      appHeader: Header,
+      Servers,
+      'app-server-details': ServerDetails,
+      'app-footer': Footer
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Home from './home.vue'
 
-Vue.component('app-servers', Home)
 
 new Vue({
   el: '#app',
