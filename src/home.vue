634795a8a72c44881f567767005d927d141c1c75
ibtehalahmed <ibtehal599@gmail.com>
2017-04-04 14:04:20 +0200

    working with components

---
+++ src/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <server-status v-for="server in 5" :key="server"></server-status>
+    </div>
+</template>
+
+<script>
+    import ServerStatus from './server_status.vue'
+    export default {
+        components: {
+            'server-status': ServerStatus
+        }
+    }
+</script>
